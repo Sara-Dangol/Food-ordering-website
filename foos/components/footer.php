0a7106ce7629bebe4e0ccfbc39a68c3acec5cc98
Sara-Dangol <153988716+Sara-Dangol@users.noreply.github.com>
2024-09-01 11:25:19 +0545

    Add files via upload

---
+++ foos/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Contacts us </h3>
+         <h5> fantastic@gmail.com</h5>
+         <h5> sam@gmail.com</h5>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>7:00am to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Address</h3>
+         <a href="#">Dillibazar, Kathmandu </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <h5>9857643216</h5>
+         <h5>9834657289</h5>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span> Sara Singh Dangol</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
